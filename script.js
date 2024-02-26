b1f135949a7385247a6f09f2858110864a992fdd
aurel-shael <deshyesguy2@gmail.com>
2024-02-26 23:34:00 +0100

    task

--- script.js
+++ script.js
@@ -40,5 +40,14 @@ taskForm.addEventListener("submit", (e) => {
     description: descriptionInput.value,
   };
 
-  
+   if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  taskData.forEach(({id, title, date, description}) => {
+      (tasksContainer.innerHTML += `
+          
+      `)
+    }
+  );
 });
