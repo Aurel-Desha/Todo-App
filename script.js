0a7763dfb1886ea8ceb506dc95466d0f2f7aaa19
aurel-shael <deshyesguy2@gmail.com>
2024-03-02 00:02:45 +0100

    editTask function

--- script.js
+++ script.js
@@ -91,6 +91,7 @@ openTaskFormBtn.addEventListener("click", () =>
 
 closeTaskFormBtn.addEventListener("click", () => {
   const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
 
   if (formInputsContainValues) {
     confirmCloseDialog.showModal();
@@ -106,7 +107,6 @@ discardBtn.addEventListener("click", () => {
   reset()
 });
 
-//event
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
